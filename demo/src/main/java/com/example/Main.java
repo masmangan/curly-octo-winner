2adf76fe266cdf3eb5155ab44c4cb5a5d8d1099a
Marco <marco.mangan@gmail.com>
2025-10-30 18:24:49 +0000

    Incluir final

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -6,13 +6,17 @@ import static java.lang.System.out;
 /**
  * A classe Hello é um exemplo simples, que é a solução do Bee1000!
  */
-public class Main {
+public final class Main {
+
+    private Main() {
+
+    }
 
     public static String greetings() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         out.println(greetings());
     }
 
